3e7d424c2abfea62a273f89cb663073c87139ffd
Tparfaite <parfaitetwagira@gmail.com>
2022-09-25 22:27:53 +0200

    Feat:donnecting database -Add the connection -Retrieve data from table [Finishes#]

--- index.php
+++ index.php
@@ -1,20 +1,49 @@
 <?php
 
-// include('form.php');
-// echo "<br>";
+#connect to database
 
-include('header.php');
-echo "<br>";
+$connect=mysqli_connect('localhost','Parfaite','Perfect@S1','personal-info');
+if(!$connect){
+    echo "connection error: " .mysqli_connect_error();
+}
 
+$query='SELECT * FROM person';
+$result=mysqli_query($connect,$query);
+$persons=mysqli_fetch_all($result,MYSQLI_ASSOC);
 
-include('form2.php');
+mysqli_free_result($result);
+mysqli_close($connect);
+
+print_r($persons);
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+  
 
 
-<!-- <?php include('header.php') ?> -->
+  <?php include('form2.php'); ?>
+  <h4>personal information</h4>
+  <div class="cards">
+  <?php foreach($persons as $person){ ?>
+  <div class="card bg-primary">
+ 
+        <div class="card-header bg-danger">
+          <?php echo "My password is " .$person['password'] ;?>
+        </div>
+        <div class="card-body">
+          <?php  echo $person['firstName']; ?>
+          <?php echo $person['lastName'] ; ?>
+        </div>
+        <div class="card-footer">
+            <?php echo $person['email']; ?>
+        </div>
+       
+  </div>
+   <?php } ?>
+  </div>
+
     
-</body>
+ 
 </html>
